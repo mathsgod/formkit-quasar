e8fc41ef27f0bdacb17ecd13b102e48dbc2a4598
Raymond Chong <mathsgod@yahoo.com>
2023-12-29 17:34:08 +0800

    🔨 重構(Toggle.vue)：移除未使用的程式碼和變數 🔥 移除(Toggle.vue)：刪除不必要的錯誤訊息註冊相關程式碼

--- src/components/Toggle.vue
+++ src/components/Toggle.vue
@@ -1,26 +1,15 @@
 <script setup>
-import registerErrorMessage from '../utils/registerErrorMessage';
 import { computed, ref } from 'vue'
 const props = defineProps({
     context: Object
 });
-
-let error = ref(false);
-let errorMessage = ref(null);
-
-registerErrorMessage(props.context.node, error, errorMessage);
 
 const value = computed({
     get: () => props.context.value,
     set: (val) => props.context.node.input(val)
 })
 
-const ss = Object.entries(useSlots()).map(([key, value]) => {
-    return key;
-});
-
 </script>
 <template>
-    <q-toggle v-model="value" :label="context.label" v-bind="context.attrs" :error="error"
-        :error-message="errorMessage"></q-toggle>
+    <q-toggle v-model="value" :label="context.label" v-bind="context.attrs"></q-toggle>
 </template>
